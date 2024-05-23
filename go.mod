ffe0f835cd569b6327de909cd5d1e4f46cbba486
Linxhhh <1599717300@qq.com>
2024-05-23 19:41:30 +0800

    配置CORS

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1 // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/gin-contrib/sessions v1.0.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"strings"
+	"time"
+
 	"github.com/Linxhhh/webook/internal/app"
 	"github.com/Linxhhh/webook/internal/app/middleware"
 	"github.com/Linxhhh/webook/internal/repository"
 	"github.com/Linxhhh/webook/internal/repository/dao"
 	"github.com/Linxhhh/webook/internal/service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/redis"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -27,15 +30,31 @@ func main() {
 func initRouter() *gin.Engine {
 	router := gin.Default()
 
-	// 注册会话中间件
+	/* 注册会话中间件
 	store, err := redis.NewStore(16, "tcp", "localhost:6379", "", []byte("sgpLG7yh8mUYnh619gO0P5HdYftPKpAQ"), []byte("FlIESLxvbN5wiYZS6v7HgLkqsTmED0yh"))
 	if err != nil {
 		panic(err)
 	}
 	router.Use(sessions.Sessions("ssid", store))
+	*/
 
 	// 注册鉴权中间件
 	router.Use(middleware.AuthByJWT())
+
+	// 配置 CORS
+	router.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowHeaders:     []string{"Content-Type", "jwt-token"},
+		ExposeHeaders:    []string{"jwt-token"},
+		AllowOriginFunc: func(origin string) bool {
+			// 开发环境下允许
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "webook.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	return router
 }
